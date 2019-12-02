9407c0bc62615bb64e57bfcca3ce91547f6e7da7
olimar718 <bencathelif@gmail.com>
2019-12-02 15:58:39 -0700

    Updated readme + modified script + udpated pdf

--- BenchmarkScriptStress.sh
+++ BenchmarkScriptStress.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-staticpriority=99
-
-resultfile="result_niceness_increase_other.txt"
 frequency=1.7
+NormalFrequency=2.7
+# resultfile="result_niceness_increase_other.txt"
+resultfile="result_rr_idle"
+
+
 time_to_s(){
   executiontime=$1
   calcstring="$(echo $executiontime | grep -Eo '[0-9]{1,3}m' | grep -Eo '[0-9]')"
@@ -29,17 +31,15 @@ sudo echo
 stress -c 4 & #loading the system massively with stresses (4 because I have a 4 core processor)
 for pid in $(pidof stress)
 do
-  # sudo chrt --idle -p 0 $pid
-  :
+sudo chrt --idle -p 0 $pid
+  # :
 done
 
 
 
-commandrr='sudo chrt --rr -p '$staticpriority' $BASHPID
-{ time ./increment >/dev/null; } |&  grep -E real | grep -Eo "[0-9]{1}m[0-9]{1,2}.[0-9]{3}"'
 
-commandfifo='sudo chrt --fifo -p '$staticpriority' $BASHPID
-time ./increment'
+
+
 
 commandbatch='sudo chrt --batch -p 0 $BASHPID
 time ./increment'
@@ -48,21 +48,40 @@ commanddeadline='sudo chrt --deadline -p 99 $BASHPID
 time ./increment'
 
 
-# echo "sched_RR results : "
-# bash <<< $commandrr # new bash session to avoid setting realtime stresses which would block the system
-
-
-for niceness in `seq -19 20`;
+# RR loop
+for staticpriority in $(seq 1 99);
 do
-  commandother='sudo chrt --other -p 0 $BASHPID
-  sudo renice --priority '$niceness' --pid $BASHPID > /dev/null
+  commandrr='sudo chrt --rr -p '$staticpriority' $BASHPID
   { time ./increment >/dev/null; } |&  grep -E real | grep -Eo "[0-9]{1}m[0-9]{1,2}.[0-9]{3}"'
-  # echo 'sched_OTHER results, niceness '$niceness' : '
-  result=$(bash <<< $commandother)  #new bash session to avoid setting realtime stresses which would block the system
+  result=$(bash <<< $commandrr)  #new bash session to avoid setting realtime stresses which would block the system
   second=$(time_to_s \'$result\')
   ratio=$(bc -l <<< "$second / $seconds_standart_executiontime")
   echo $ratio >> $resultfile
 done
 
+# # FIFO loop
+# for staticpriority in $(seq 1 99);
+# do
+#   commandfifo='sudo chrt --fifo -p '$staticpriority' $BASHPID
+#   { time ./increment >/dev/null; } |&  grep -E real | grep -Eo "[0-9]{1}m[0-9]{1,2}.[0-9]{3}"'
+#   result=$(bash <<< $commandfifo)  #new bash session to avoid setting realtime stresses which would block the system
+#   second=$(time_to_s \'$result\')
+#   ratio=$(bc -l <<< "$second / $seconds_standart_executiontime")
+#   echo $ratio >> $resultfile
+# done
+
+## Other LOOP
+# for niceness in `seq -19 20`;
+# do
+#   commandother='sudo chrt --other -p 0 $BASHPID
+#   sudo renice --priority '$niceness' --pid $BASHPID > /dev/null
+#   { time ./increment >/dev/null; } |&  grep -E real | grep -Eo "[0-9]{1}m[0-9]{1,2}.[0-9]{3}"'
+#   result=$(bash <<< $commandother)  #new bash session to avoid setting realtime stresses which would block the system
+#   second=$(time_to_s \'$result\')
+#   ratio=$(bc -l <<< "$second / $seconds_standart_executiontime")
+#   echo $ratio >> $resultfile
+# done
+
 
 killall stress # removing stresses so that your computer doesn't become a airplane
+sudo cpupower frequency-set --max $NormalFrequency\GHz > /dev/null
